a2a4092277f2c80c0601d0a32f1a845b51bf3238
heecheol <heefe92@naver.com>
2018-09-14 14:52:21 +0900

    haha

---
+++ BinaryClassificationModel/config.py
@@ -0,0 +1,19 @@
+
+class Config:
+    # data
+    data_dir = 'F:/DataSet/VOC2007/'
+    num_workers = 8
+
+
+    # param for optimizer
+    lr = 1e-4
+
+
+    # training
+    epoch = 100
+    batch_size=1
+
+
+
+
+opt = Config()
--- BinaryClassificationModel/train.py
+++ BinaryClassificationModel/train.py
@@ -1,11 +1,14 @@
 import numpy as np
 import model
+import collections
 
 from .trainer import Trainer
 import torch as t
 import torch.optim as optim
 from .dataset import Dataset
+from torch.utils import data as data_
 import cv2,time
+from .config import opt
 
 def run_train(train_verbose=False):
     pass
@@ -38,7 +41,7 @@ def run_train(train_verbose=False):
     num_bad_epochs = 0
     max_bad_epochs = 5
 
-    for epoch_num in range(10):
+    for epoch_num in range(opt.epoch):
         my_trainer.train_mode()
         train_start_time = time.time()
         train_epoch_loss = []
@@ -79,13 +82,13 @@ def run_train(train_verbose=False):
             best_loss = np.mean(vali_epoch_loss)
             best_map_epoch_num = epoch_num
             num_bad_epochs = 0
-            resnet_trainer.model_save(epoch_num)
+            my_trainer.model_save(epoch_num)
         if (num_bad_epochs > max_bad_epochs):
             num_bad_epochs = 0
-            my_trainer.model_load(best_map_epoch_num)
+            my_trainer.model_load(best_loss_epoch_num)
             my_trainer.reduce_lr(factor=0.1, verbose=True)
 
-        print('best epoch num', best_map_epoch_num)
+        print('best epoch num', best_loss_epoch_num)
         print('----------------------------------------')
 
     print(epoch_loss_hist)
